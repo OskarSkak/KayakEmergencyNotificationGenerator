18c140bf322f4851a0f19ea02280f8660f5e20db
Casper Hansen <caspha17@gmail.com>
2020-10-10 10:32:32 +0200

    Added gps screen

--- kayakinsights/src/screens/GpsSensorScreen.js
+++ kayakinsights/src/screens/GpsSensorScreen.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
+const  GpsSensorScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
 
-const GpsSensorScreen = ({navigation}) => {
-    return (
-        <View style={styles.container} >
-            <Pressable onPress={() => navigation.navigate('gyro')} >
-            <Text>GPS Sensor</Text>
-            </Pressable>
-        </View>
-    )
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-        
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
 export default GpsSensorScreen;
